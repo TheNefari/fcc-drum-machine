7fc7e6798af38b99333562498c473a1147e9ac05
Robert Müller <55889196+TheNefari@users.noreply.github.com>
2019-11-17 21:05:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ class DRUMMACHINE extends React.Component {
     let clip = document.getElementById(clipId);
 
     clip.play();
-    this.setState({ clip: clipId, isPlaying: true, key: clipId });
+    this.setState({ clip: clipId, isPlaying: true, key: clip.title });
     this.playing(clipId);
   }
   playing(clipId) {
@@ -58,7 +58,6 @@ class DRUMMACHINE extends React.Component {
     let buttons = document.getElementsByClassName("drum-pad");
     window.addEventListener("mousedown", this.onMousePressed);
     window.addEventListener("mouseup", this.onMouseUp);
-    //});
 
   }
 
@@ -68,31 +67,31 @@ class DRUMMACHINE extends React.Component {
       React.createElement("div", { id: "drum-machine", onKeyDown: this.onKeyPressed },
       React.createElement("div", { id: "pad-field" },
       React.createElement("button", { id: "button1", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Gotcha+bitch!+Sound+Effect+(-Got+you-)&filename=22/224470-9529e593-7aa8-4041-b714-18e2b1343cd0.mp3", id: "Q", class: "clip" }), "Q"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Gotcha+bitch!+Sound+Effect+(-Got+you-)&filename=22/224470-9529e593-7aa8-4041-b714-18e2b1343cd0.mp3", id: "Q", class: "clip", title: "Gotcha" }), "Q"),
 
       React.createElement("button", { id: "button2", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=MLG+Horns+Sound+Effect&filename=22/224470-81e5802f-e44c-4a89-8eef-518ebd1f4424.mp3", id: "W", class: "clip" }), "W"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=MLG+Horns+Sound+Effect&filename=22/224470-81e5802f-e44c-4a89-8eef-518ebd1f4424.mp3", id: "W", class: "clip", title: "Horn" }), "W"),
 
       React.createElement("button", { id: "button3", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Really+Nigga+Sound+Effect&filename=22/224470-ef2650d4-4b94-4435-ac5f-79bf2e44b34e.mp3", id: "E", class: "clip" }), "E"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Really+Nigga+Sound+Effect&filename=22/224470-ef2650d4-4b94-4435-ac5f-79bf2e44b34e.mp3", id: "E", class: "clip", title: "Really" }), "E"),
 
       React.createElement("button", { id: "button4", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Suprise+Motherf-cker+Sound+Effect&filename=22/224470-a2c4de5b-c03e-4a6f-a112-d6a0b9492c91.mp3", id: "A", class: "clip" }), "A"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Suprise+Motherf-cker+Sound+Effect&filename=22/224470-a2c4de5b-c03e-4a6f-a112-d6a0b9492c91.mp3", id: "A", class: "clip", title: "Suprise" }), "A"),
 
       React.createElement("button", { id: "button5", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=1069er+we+got+whores&filename=mj/MjIzMTEyOTcyMjMxMjI_4Q8ov5l4jf8.mp3", id: "S", class: "clip" }), "S"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=1069er+we+got+whores&filename=mj/MjIzMTEyOTcyMjMxMjI_4Q8ov5l4jf8.mp3", id: "S", class: "clip", title: "1069er" }), "S"),
 
       React.createElement("button", { id: "button6", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=I%27ve+got+balls+of+steel&filename=nt/NTYyMjExMTAzNTYyMjg4_XqoJirdnKMM.mp3", id: "D", class: "clip" }), "D"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=I%27ve+got+balls+of+steel&filename=nt/NTYyMjExMTAzNTYyMjg4_XqoJirdnKMM.mp3", id: "D", class: "clip", title: "I got BoS" }), "D"),
 
       React.createElement("button", { id: "button7", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Balls+of+steel&filename=nd/NDgwNTExMTAzNDgwNTcw_F_2fn6gVelx88.mp3", id: "Z", class: "clip" }), "Z"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Balls+of+steel&filename=nd/NDgwNTExMTAzNDgwNTcw_F_2fn6gVelx88.mp3", id: "Z", class: "clip", title: "BoS" }), "Z"),
 
       React.createElement("button", { id: "button8", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Balls&filename=mt/MTA0NTExMTAzMTA0NTQ4_g2wol7gthU0.mp3", id: "X", class: "clip" }), "X"),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Balls&filename=mt/MTA0NTExMTAzMTA0NTQ4_g2wol7gthU0.mp3", id: "X", class: "clip", title: "Balls" }), "X"),
 
       React.createElement("button", { id: "button9", class: "drum-pad", onClick: this.onClickHandler },
-      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Steel&filename=nd/NDE4NDExMTAzNDE4NDM3_mr9GRQFlG9E.mp3", id: "C", class: "clip" }), "C")),
+      React.createElement("audio", { src: "https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Steel&filename=nd/NDE4NDExMTAzNDE4NDM3_mr9GRQFlG9E.mp3", id: "C", class: "clip", title: "Steel" }), "C")),
 
 
       React.createElement("p", { id: "display" }, this.state.key)),
